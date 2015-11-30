1be7f300b5832cc0219110f2a8794c4826c0d043
lucia <analucia0189@gmail.com>
2015-11-30 17:11:37 -0600

    algo

--- client/supportChat.js
+++ client/supportChat.js
@@ -1,16 +1,20 @@
 Template.chatClient.events({
 	'keypress textarea': function(ev) {
-      var $textarea = $('.input textarea');
-      if (ev.which === 13 && $textarea.val().trim() !== ''){
+      var textarea = $('.input textarea').val();
+      if (ev.which === 13 && textarea.trim() !== ''){
         ev.stopPropagation();
         //Meteor.call('insertMessage', {content: $textarea.val(), channel: Session.get('channel')})
+        Messages.insert({
+        	body:textarea
+        });
+
         $textarea.val('');
       }
     }
 });
 
 Template.chatClient.helpers({
-	'messeges': function(){
+	'messages': function(){
 		return Messages.find();
 	}
 });
