5d88ce152c712635896d45a35aee4104866d5168
lucia <analucia0189@gmail.com>
2015-12-02 16:15:10 -0600

    almost

--- client/supportChat.js
+++ client/supportChat.js
@@ -1,18 +1,16 @@
-Template.chatClient.events({
-	'keypress textarea': function(ev) {
-      var textarea = $('.input textarea').val();
-      if (ev.which === 13 && textarea.trim() !== ''){
-        ev.stopPropagation();
-        Messages.insert({
-        	body:textarea
-        });
-        $('.input textarea').val('');
-      }
-    }
+Template.chat.helpers({
+	chat: function () {
+		return Chat.find();
+	}
 });
 
-Template.chatClient.helpers({
-	'messages': function(){
-		return Messages.find();
-	}
+Template.chat.events({
+	'keypress textarea': function(ev) {
+      var $textarea = $('.input textarea')
+      if (ev.which === 13 && $textarea.val().trim() !== ''){
+        ev.stopPropagation();
+        Chat.insert({user:"Support", message:$textarea.val() });
+        $textarea.val('');
+      }
+    }
 });
