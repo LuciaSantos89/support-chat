ccca953d9616595f6cbead10f38ef120780d19b7
lucia <analucia0189@gmail.com>
2015-12-08 15:58:49 -0600

    ya casi

--- client/supportChat.js
+++ client/supportChat.js
@@ -1,6 +1,6 @@
 Template.chat.helpers({
-	chat: function () {
-		return Chat.find();
+	chat:function(){
+		return Session.get('chat');
 	},
 	clients:function(){
 		return Clients.find();
@@ -8,12 +8,18 @@ Template.chat.helpers({
 });
 
 Template.chat.events({
-	'keypress textarea': function(ev) {
+	'keypress textarea': function(event) {
       var $textarea = $('.input textarea')
-      if (ev.which === 13 && $textarea.val().trim() !== ''){
-        ev.stopPropagation();
-        Chat.insert({user:"Support", message:$textarea.val(), to:"" });
+      if (event.which === 13 && $textarea.val().trim() !== ''){
+        event.stopPropagation();
+        Chat.insert({user:"Support", message:$textarea.val(), to:Session.get('clientId') });
         $textarea.val('');
       }
+    },
+    'click li.client':function(event){
+    	var clientId = event.target.id;
+    	var chat = Chat.find({user:clientId}).fetch();
+    	Session.set('clientId',clientId);
+    	Session.set('chat',chat);
     }
 });
