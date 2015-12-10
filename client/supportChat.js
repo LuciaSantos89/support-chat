6cf2ac1de91669cc15c9773dbea2b3e9d53559e8
lucia <analucia0189@gmail.com>
2015-12-10 13:11:03 -0600

    terminado

--- client/supportChat.js
+++ client/supportChat.js
@@ -1,25 +1,31 @@
 Template.chat.helpers({
-	chat:function(){
-		return Session.get('chat');
-	},
-	clients:function(){
-		return Clients.find();
-	}
+    chat: function() {
+        if (Session.equals("clientId", undefined)) {
+            return [];
+        } else {
+            return Chat.find({$or: [{user: Session.get('clientId')}, {to: Session.get('clientId')}]});
+        }
+    },
+    clients: function() {
+        return Clients.find();
+    }
 });
 
 Template.chat.events({
-	'keypress textarea': function(event) {
-      var $textarea = $('.input textarea')
-      if (event.which === 13 && $textarea.val().trim() !== ''){
-        event.stopPropagation();
-        Chat.insert({user:"Support", message:$textarea.val(), to:Session.get('clientId') });
-        $textarea.val('');
-      }
+    'keypress textarea': function(event) {
+        var $textarea = $('.input textarea')
+        if (event.which === 13 && $textarea.val().trim() !== '') {
+            event.stopPropagation();
+            Chat.insert({
+                user: "Support",
+                message: $textarea.val(),
+                to: Session.get('clientId')
+            });
+            $textarea.val('');
+        }
     },
-    'click li.client':function(event){
-    	var clientId = event.target.id;
-    	var chat = Chat.find({$or:[{user:clientId}, {to:clientId}]}).fetch();
-    	Session.set('clientId',clientId);
-    	Session.set('chat',chat);
+    'click li.client': function(event) {
+        var clientId = event.target.id;
+        Session.set('clientId', clientId);
     }
 });
